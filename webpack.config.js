8cf818225e2094096237981602bc4d8ebdc7147f
Keanu Lee <keanulee@google.com>
2018-06-06 15:54:38 -0700

    Webpack concept allowing dev without building

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: {
+    'node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter': '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js',
+    'node_modules/@webcomponents/webcomponentsjs/webcomponents-bundle': '@webcomponents/webcomponentsjs/webcomponents-bundle.js',
+    'node_modules/@webcomponents/webcomponentsjs/webcomponents-loader': '@webcomponents/webcomponentsjs/webcomponents-loader.js',
+    'node_modules/@webcomponents/webcomponentsjs/bundles/webcomponents-ce': '@webcomponents/webcomponentsjs/bundles/webcomponents-ce.js',
+    'node_modules/@webcomponents/webcomponentsjs/bundles/webcomponents-sd': '@webcomponents/webcomponentsjs/bundles/webcomponents-sd.js',
+    'node_modules/@webcomponents/webcomponentsjs/bundles/webcomponents-sd-ce': '@webcomponents/webcomponentsjs/bundles/webcomponents-sd-ce.js',
+    'node_modules/@webcomponents/webcomponentsjs/bundles/webcomponents-sd-ce-pf': '@webcomponents/webcomponentsjs/bundles/webcomponents-sd-ce-pf.js',
+    main: './main.js'
+  },
+  mode: 'production'
+};
